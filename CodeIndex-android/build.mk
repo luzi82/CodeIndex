7a02a7847afe672cdc5e5b396882a2fdc17ea481
luzi82 <luzi82@gmail.com>
2012-12-26 19:56:35 +0800

    change app icon

--- CodeIndex-android/build.mk
+++ CodeIndex-android/build.mk
@@ -11,7 +11,13 @@ DEPS_PATH = deps
 TOOL_I18N_PATH = ${DEPS_PATH}/i18n
 TOOL_JNIVERSION_PATH = ${DEPS_PATH}/jni_version
 
-all : .i18n_timestamp libs/armeabi/lib${JNILIB}.so
+all : .i18n_timestamp libs/armeabi/lib${JNILIB}.so \
+	ic_launcher-web.png \
+	res/drawable-xhdpi/ic_launcher.png \
+	res/drawable-mdpi/ic_launcher.png \
+	res/drawable-hdpi/ic_launcher.png \
+	res/drawable-ldpi/ic_launcher.png
+
 
 .i18n_timestamp : src2/i18n/i18n.ods
 	java \
@@ -28,7 +34,32 @@ libs/armeabi/lib${JNILIB}.so : jni/${JNILIB}.c jni/Android.mk
 		-t ${TOOL_JNIVERSION_PATH}/jni_version.xml
 	${NDK_PATH}/ndk-build APP_PLATFORM=${APP_PLATFORM}
 
+src2/icon/icon.png : src2/icon/icon.xcf
+	xcf2png src2/icon/icon.xcf -o src2/icon/icon.png
+
+ic_launcher-web.png : src2/icon/icon.png
+	convert src2/icon/icon.png -resize 512x512 ic_launcher-web.png
+	
+res/drawable-xhdpi/ic_launcher.png : src2/icon/icon.png
+	convert src2/icon/icon.png -resize 96x96 res/drawable-xhdpi/ic_launcher.png
+
+res/drawable-mdpi/ic_launcher.png : src2/icon/icon.png
+	convert src2/icon/icon.png -resize 48x48 res/drawable-mdpi/ic_launcher.png
+
+res/drawable-hdpi/ic_launcher.png : src2/icon/icon.png
+	convert src2/icon/icon.png -resize 72x72 res/drawable-hdpi/ic_launcher.png
+
+res/drawable-ldpi/ic_launcher.png : src2/icon/icon.png
+	convert src2/icon/icon.png -resize 36x36 res/drawable-ldpi/ic_launcher.png
+
 clean :
 	rm -rf .i18n_timestamp
 	rm -rf libs/armeabi
 	rm -rf jni/version.h
+	rm -rf \
+		src2/icon/icon.png \
+		ic_launcher-web.png \
+		res/drawable-xhdpi/ic_launcher.png \
+		res/drawable-mdpi/ic_launcher.png \
+		res/drawable-hdpi/ic_launcher.png \
+		res/drawable-ldpi/ic_launcher.png
